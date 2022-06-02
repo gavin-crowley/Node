ccf8da0fd0d8b95c9d2d67aec8844047caa686e1
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-06-02 19:14:28 +0100

    env file setup

--- Smilga/my-node-course/4-task-manager/db/connect.js
+++ Smilga/my-node-course/4-task-manager/db/connect.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://gavin:test1234@mynodeexpressprojects.gghmk.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
 
 const connectDB = (url) => {
-  return mongoose.connect(connectionString, {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
