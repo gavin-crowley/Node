3b681a599e4025686fc237a7502df7d4eaee5f0e
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-06-02 19:08:45 +0100

    refactor connect

--- Smilga/my-node-course/4-task-manager/app.js
+++ Smilga/my-node-course/4-task-manager/app.js
@@ -2,6 +2,7 @@ require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
 
 // middleware
 app.use(express.json())
@@ -15,4 +16,13 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3000
 
-app.listen(port, console.log(`server is listening on port ${port}...`))
+const start = async () => {
+    try {
+        await connectDB()
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
--- Smilga/my-node-course/4-task-manager/db/connect.js
+++ Smilga/my-node-course/4-task-manager/db/connect.js
@@ -3,16 +3,13 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://gavin:test1234@mynodeexpressprojects.gghmk.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("CONNECTED TO THE DB...");
-  })
-  .catch((err) => {
-    console.log(err);
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
   });
+};
+
+module.exports = connectDB
